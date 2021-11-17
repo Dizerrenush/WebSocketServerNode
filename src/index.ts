4cf69e67f866bb0e1fb86f464e311828d4d15b45
Dmitriy Zhernov <dima555zhernov@gmail.com>
2021-11-17 08:59:22 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,20 @@ const app = express();
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wsServer = new WebSocket.Server({ server });
 
 const port = process.env.PORT || 3000;
 
-wss.on('connection', (ws: WebSocket) => {
+wsServer.on('connection', (ws: WebSocket) => {
 
     ws.on('message', (message: string) => {
 
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        wsServer.clients.forEach(client => client.send(message));
     });
 
-    ws.send('Hi there, I am a WebSocket server');
 });
 
 server.listen(port, () => {
     // @ts-ignore
-    console.log(`Server started on port ${server.address().port} :)`);
+    console.log(`Server started on port ${server.address().port}`);
 });
